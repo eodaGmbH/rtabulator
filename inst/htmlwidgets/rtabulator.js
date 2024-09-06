e574fab160744c381275b837d96a8b004fc3349f
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-09-06 09:47:16 +0200

    Use built js code

--- inst/htmlwidgets/rtabulator.js
+++ inst/htmlwidgets/rtabulator.js
@@ -1,28 +1,17 @@
-HTMLWidgets.widget({
-
-  name: 'rtabulator',
-
-  type: 'output',
-
-  factory: function(el, width, height) {
-
-    // TODO: define shared variables for this instance
-
-    return {
-
-      renderValue: function(x) {
-
-        // TODO: code to render the widget, e.g.
-        el.innerText = x.message;
-
-      },
-
-      resize: function(width, height) {
-
-        // TODO: code to re-render the widget with a new size
-
-      }
-
-    };
+(() => {
+  // srcjs/index-r.js
+  function tabulatorFactory(widgetElement, width, height) {
+    let table = null;
+    function renderValue(widgetData) {
+      console.log(widgetData);
+    }
+    function resize(width2, height2) {
+    }
+    return { renderValue, resize };
   }
-});
+  HTMLWidgets.widget({
+    name: "rtabulator",
+    type: "output",
+    factory: tabulatorFactory
+  });
+})();
