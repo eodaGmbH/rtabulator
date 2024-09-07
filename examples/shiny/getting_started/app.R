2e3e353af2754df5eb31acd97af9de1fafbf28e0
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-09-07 09:36:03 +0200

    Listen on input

--- examples/shiny/getting_started/app.R
+++ examples/shiny/getting_started/app.R
@@ -2,12 +2,18 @@ library(shiny)
 
 ui <- fluidPage(
   titlePanel("Tabulator"),
-  tabulatorOutput("table")
+  tabulatorOutput("table"),
+  textOutput("row")
 )
 
 server <- function(input, output) {
   output$table <- renderTabulator({
     tabulator(iris, table_options())
+  })
+
+  output$row <- renderText({
+    print(input$table_row_clicked)
+    input$table_row_clicked$Sepal_Length
   })
 }
 
