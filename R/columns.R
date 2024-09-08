872434ec2827efa2bc6328bb7068fc4487848025
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-09-08 18:49:33 +0200

    Move funcs

--- R/columns.R
+++ R/columns.R
@@ -1,3 +1,30 @@
+fix_colnames <- function(data) {
+  colnames(data) <- sub("\\.", "_", colnames(data))
+  return(data)
+}
+
+create_columns <- function(data) {
+  data <- fix_colnames(data)
+  dtype_is_numeric <- sapply(data, is.numeric)
+  purrr::map(
+    colnames(data),
+    ~ list(
+      title = .x,
+      field = .x,
+      hozAlign = as.vector(ifelse(dtype_is_numeric[.x], "right", "left"))
+    )
+  )
+}
+
+set_auto_id <- function(data) {
+  if ("id" %in% colnames(data)) {
+    return(data)
+  }
+
+  data$id <- seq(1:nrow(data))
+  return(data)
+}
+
 # TODO: Add possibility to select specific columns
 add_editor_to_columns <- function(columns, data = NULL) {
   if (is.null(data)) {
--- R/utils.R
+++ R/utils.R
@@ -1,10 +1,3 @@
-# Avoid duplicates
-# utils::modifyList
-# merge_lists <- function(l1, l2) {
-#  for (key in names(l2)) l1[[key]] <- NULL
-#  c(compact(l1), l2)
-# }
-
 to_camel_case <- function(x) {
   gsub("_(\\w?)", "\\U\\1", x, perl = TRUE)
 }
@@ -16,31 +9,4 @@ keys_to_camel_case <- function(x) {
 # Remove NULL values from list
 compact <- function(x) {
   x[!sapply(x, is.null)]
-}
-
-fix_colnames <- function(data) {
-  colnames(data) <- sub("\\.", "_", colnames(data))
-  return(data)
-}
-
-create_columns <- function(data) {
-  data <- fix_colnames(data)
-  dtype_is_numeric <- sapply(data, is.numeric)
-  purrr::map(
-    colnames(data),
-    ~ list(
-      title = .x,
-      field = .x,
-      hozAlign = as.vector(ifelse(dtype_is_numeric[.x], "right", "left"))
-    )
-  )
-}
-
-set_auto_id <- function(data) {
-  if ("id" %in% colnames(data)) {
-    return(data)
-  }
-
-  data$id <- seq(1:nrow(data))
-  return(data)
 }
