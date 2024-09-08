5bf9b94615666098f950a67a6bfc0fce5555866b
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-09-08 18:47:17 +0200

    styler

--- R/tabulator_options.R
+++ R/tabulator_options.R
@@ -5,38 +5,30 @@ tabulator_options <- function(
     # General
     height = NULL,
     history = FALSE, # Must be TRUE in order to use 'undo' and 'redo'
-
     # Columns
     columns = NULL,
     layout = c("fitColumns", "fitData", "fitDataFill", "fitDataStretch", "fitDataTable"),
     responsive_layout = FALSE,
     movable_columns = FALSE,
     header_visible = TRUE,
-
     # Rows
     row_height = NULL,
     add_row_pos = c("bottom", "top"),
     movable_rows = FALSE,
     resizable_rows = FALSE,
     frozen_rows = NULL,
-
     # Data
     index = "id",
-
     # Row Grouping
     group_by = NULL,
-
     # Editing
     edit_trigger_event = "dblclick",
-
     # Selection
     selectable_rows = "highlight", # 'highlight', bool or integer
-
     # Pagination
     pagination = FALSE,
     paginations_size = 10,
     pagination_add_row = "page",
-
     # Spreadsheet
     spreadsheet = FALSE,
     spreadsheet_rows = NULL,
