08861e8ea5fa13d3804f8a8db9c038167ebedc08
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-09-09 15:36:39 +0200

    Add themes to param vector

--- R/themes.R
+++ R/themes.R
@@ -1,4 +1,5 @@
-read_tabulator_theme <- function(theme = c("midnight", "modern", "simple", "site")) {
+read_tabulator_theme <- function(
+    theme = c("midnight", "modern", "simple", "site", "bootstrap3", "bootstrap4", "bootstrap5", "bulma", "materialize", "semanticui")) {
   theme <- match.arg(theme)
   file_name <- system.file(
     glue::glue("htmlwidgets/libs/tabulator/tabulator_{theme}.min.css"),
