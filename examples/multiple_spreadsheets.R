6430b67ec91c2f849dbd6d360c2c16166fda7dfb
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-09-10 17:03:59 +0200

    Add example with multiple sheets

---
+++ examples/multiple_spreadsheets.R
@@ -0,0 +1,16 @@
+options <- tabulator_options(
+  spreadsheet = TRUE,
+  spreadsheet_sheets = list(
+    list(
+      title = "First",
+      data = list(c(1, 2, 3))
+    ),
+    list(
+      title = "Second",
+      data = list(c(4, 5, 6))
+      )
+    ),
+  spreadsheet_sheet_tabs=TRUE
+)
+
+tabulator(data = NULL, options, theme = "midnight")
