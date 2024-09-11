ef04a9d1a6aafb5ffe7649250a3f4cea5aba2bd6
Stefan Kuethe <stefan.kuethe@eoda.de>
2024-09-11 08:23:45 +0200

    Add formatter example

--- examples/formatters.R
+++ examples/formatters.R
@@ -1,2 +1,3 @@
 tabulator("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv") |>
-  set_star_formatter("Pclass", number_of_stars = 3)
+  set_star_formatter("Pclass", number_of_stars = 3) |>
+  set_tick_cross_formatter("Survived")
