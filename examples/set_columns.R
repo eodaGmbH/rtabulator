148b68998594bc2790a9f87efbef7dccfc81270d
friessn <nico.friess@posteo.de>
2024-09-26 11:20:20 +0200

    styler

--- examples/set_columns.R
+++ examples/set_columns.R
@@ -1,10 +1,12 @@
 t <- tabulator(
   data.frame(
-    numbers = c(1,11,3),
-    text = c("How","are","you"),
+    numbers = c(1, 11, 3),
+    text = c("How", "are", "you"),
     colors = c("red", "green", "blue"),
-    progress = c(10,50,100),
-    johncena = c("can't", "see", "me")))
+    progress = c(10, 50, 100),
+    johncena = c("can't", "see", "me")
+  )
+)
 
 t |>
   # set a title and formatter with default values
@@ -13,6 +15,6 @@ t |>
   set_columns(c("numbers", "text"), hoz_align = "center") |>
   set_columns("johncena", visible = FALSE) |>
   # set formatter params in the formatter function
-  set_columns("progress", formatter = formatter_progress(max = 50))|>
+  set_columns("progress", formatter = formatter_progress(max = 50)) |>
   # use the ... argument to add parameters that might be implemented in the JS library
   set_columns("numbers", sorter = "string")
